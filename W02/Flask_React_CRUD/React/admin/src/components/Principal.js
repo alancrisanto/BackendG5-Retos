13c8ba8115781a9e43dc064fefbd911a98bc2fba
alancrisanto <alanvcrisanto@gmail.com>
2022-01-05 14:54:00 -0500

    Update 3 Busqueda por nombre

--- W02/Flask_React_CRUD/React/admin/src/components/Principal.js
+++ W02/Flask_React_CRUD/React/admin/src/components/Principal.js
@@ -1,7 +1,8 @@
 import { Offcanvas, Button, Form, Table } from "react-bootstrap"
 import {useEffect, useState} from "react"
+import Swal from "sweetalert2"
 
-import { obt_personal, agregar_personal } from "../services/obt_data";
+import { obt_personal, agregar_personal, delete_personal, update_personal, obt_persona_id } from "../services/obt_data";
 
 export default function Principal() {
 
@@ -11,27 +12,149 @@ export default function Principal() {
     const handleShow = () => setShow(true);
     
     // const for the form
-    const [nombres, setNombres] = useState("");
-    const [apellidos, setApellidos] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [apellido, setApellido] = useState("");
     const [dni, setDni] = useState("");
     const [area, setArea] = useState("");
     const [fechaIngreso, setFechaIngreso] = useState("");
 
-    const handleSubmit = (e) => {
+    // States to fill and manage data from API
+    const [users, setUsers] = useState([]);
+    const [tabla, setTabla] = useState([])
+    const[busqueda, setBusqueda] = useState("")
+
+    // This allow to edit the information of an specific id
+    const [edit, setEdit] = useState(false);
+    const [id, setId] = useState("")
+
+    const nvo_personal = {
+        nombre,
+        apellido,
+        dni,
+        area,
+        fechaIngreso,
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(agregar_personal())
+
+        if (edit === false ){
+            await agregar_personal(nvo_personal);
+            await get_personal();
+        }else {
+            await update_personal(id, nvo_personal )
+            await get_personal();
+            handleClose(true)
+            setEdit(false);
+            setId("");
+        }
+        setNombre("")
+        setApellido("")
+        setDni("")
+        setArea("")
+        setFechaIngreso("")
+        
+    }
+
+    const handleChange = (e) => {
+        buscar(e.target.value);
+        setBusqueda(e.target.value);
+    }
+
+    const buscar = (elementoDeBusqueda) => {
+        const resultado_busqueda = tabla.filter((elemento) => {
+            if (elemento.dni.toString().includes(elementoDeBusqueda) ||
+            elemento.apellido.toString().toLowerCase().includes(elementoDeBusqueda.toLowerCase()) || elemento.nombre.toString().toLowerCase().includes(elementoDeBusqueda.toLowerCase())
+            ){
+                return elemento
+            }
+        });
+        setUsers(resultado_busqueda)
+
+    }
+
+    const limpiar_campos = () => {
+        setNombre("")
+        setApellido("")
+        setDni("")
+        setArea("")
+        setFechaIngreso("")
+    }
+
+    const get_personal = async () => {
+        try {
+            const lista_personal = await obt_personal();
+            setUsers(lista_personal);
+            setTabla(lista_personal);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const borrar_personal = async (id) => {
+        const respuesta = await Swal.fire({
+            title: "Seguro que quiere eliminar a esta persona?",
+            text: "Este proceso no se puede revertir!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, borrar!',
+        })
+        if (respuesta.isConfirmed){
+            try {
+                await delete_personal(id);
+                await get_personal();
+                Swal.fire({
+                    icon: "success",
+                    title:'Personal ha sio borrado',
+                    timer: 1500
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+
+    const editar_personal = async (id) => {
+
+        handleShow(true)
+
+        const data = await obt_persona_id(id)
+        setEdit(true)
+        setId(id)
+        setNombre(data.nombre)
+        setApellido(data.apellido)
+        setDni(data.dni)
+        setArea(data.area)
+        setFechaIngreso(data.fechaIngreso)
+
+        await get_personal();
     }
 
     useEffect(()=> {
-        obt_personal()
+        get_personal()
     }, [])
+
     return (
         <div>
             <h1>Bienvenido</h1>
+            <div className="busqueda_agregar mb-3">
             <Button className="btn-agregar" variant="primary" onClick={handleShow}>
                 Agregar Personal
             </Button>
-
+                <div className="containerInput col-7">
+                    <input
+                    className="form-control inputBuscar" 
+                    value={busqueda}
+                    placeholder="Búsqueda por Nombre, Apellido o DNI"
+                    onChange={handleChange}
+                    />
+                    <button className="btn btn-success">
+                    <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
             <Offcanvas show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
                 <Offcanvas.Title>Ingresar Datos</Offcanvas.Title>
@@ -45,9 +168,9 @@ export default function Principal() {
                                 className="form-control"
                                 id="nombre"
                                 placeholder="Ej. Juan Carlos"
-                                value={nombres}
-                                onChange={e => setNombres(e.target.value)}
-                                autofocus
+                                value={nombre}
+                                onChange={e => setNombre(e.target.value)}
+                                autoFocus
                                 />
                         </div>
 
@@ -56,10 +179,10 @@ export default function Principal() {
                             <input 
                                 type="text" 
                                 className="form-control" 
-                                id="apellidos"
+                                id="apellido"
                                 placeholder="Ej. Perez Moncada"
-                                value={apellidos}
-                                onChange={e => setApellidos(e.target.value)} />
+                                value={apellido}
+                                onChange={e => setApellido(e.target.value)} />
                         </div>
 
                         <div className="mb-3">
@@ -95,8 +218,8 @@ export default function Principal() {
                                 onChange={e => setFechaIngreso(e.target.value)} />
                         </div>
                         <div className="mt-5">
-                            <button type="submit" className="btn btn-success">Submit</button>
-                            <button type="submit" className="btn btn-danger mx-3">Cancelar</button>
+                            <button type="submit" className="btn btn-success">{edit ? "Editar" : "Agregar"}</button>
+                            <button type="reset" className="btn btn-danger mx-3" onClick={limpiar_campos}>Limpiar</button>
                         </div>
                     </form>
                 </Offcanvas.Body>
@@ -115,26 +238,18 @@ export default function Principal() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>43407717</td>
-                    <td>Compras</td>
-                    <td>12/02/2015</td>
-                    <td><Button variant="success">Editar</Button></td>
-                    <td><Button variant="danger">Eliminar</Button></td>
-                    </tr>
-                    <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>12345678</td>
-                    <td>Ventas</td>
-                    <td>06/05/2018</td>
-                    <td><Button variant="success">Editar</Button></td>
-                    <td><Button variant="danger">Eliminar</Button></td>
-                    </tr>
+                    {users.map((user, i) => (
+                        <tr key={i}>
+                            <td>{i +1}</td>
+                            <td>{user.nombre}</td>
+                            <td>{user.apellido}</td>
+                            <td>{user.dni}</td>
+                            <td>{user.area}</td>
+                            <td>{user.fechaIngreso}</td>
+                            <td><Button variant="success" onClick={(e) => editar_personal(i)}>Editar</Button></td>
+                            <td><Button variant="danger" onClick={(e) => borrar_personal(i)}>Eliminar</Button></td>
+                        </tr>
+                    ))}
                 </tbody>
             </Table>
         </div>
--- W02/Flask_React_CRUD/data.py
+++ W02/Flask_React_CRUD/data.py
@@ -1,26 +1,23 @@
 personal = [
     {
-        "id": 1,
         "nombre": "kurt",
         "apellido": "cobain",
         "dni": "45612378",
         "area": "logistica",
-        "fecha_ingreso": "25/02/2015"
+        "fechaIngreso": "2015-12-19"
     },
     {
-        "id": 2,
         "nombre": "dave",
         "apellido": "grohl",
         "dni": "12345679",
         "area": "finanzas",
-        "fecha_ingreso": "02/02/2018"
+        "fechaIngreso": "2018-02-20"
     },
     {
-        "id": 3,
         "nombre": "krist",
         "apellido": "novoselic",
         "dni": "65432112",
         "area": "finanzas",
-        "fecha_ingreso": "13/06/2016"
+        "fechaIngreso": "2016-05-30"
     }
 ]
